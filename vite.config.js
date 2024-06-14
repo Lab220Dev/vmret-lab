52435472a53ebab2b0e617b38e82675cfc395af4
Gabriel Ramos <gtorres321@gmail.com>
2024-06-14 09:38:01 -0300

    correção de bigs na pagina de adicionar funcionarios

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         plugins: [vue()],
-        base:'',
+        base:'/dist/',
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
