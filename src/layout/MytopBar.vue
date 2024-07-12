6b06ec76ee2bf4abb4128ed28f9fc7a5350261dd
Lab220Dev <suporte@lab220.com.br>
2024-07-12 17:52:49 -0300

    Layout - mytopbar e mytopside
    Mudei o posicionamento da logo, do relógio e do usuário

--- src/layout/MytopBar.vue
+++ src/layout/MytopBar.vue
@@ -1,22 +1,36 @@
 <script setup>
-import { ref,  onMounted, onBeforeUnmount, reactive } from 'vue';
+import { ref, onMounted, onBeforeUnmount, reactive } from 'vue';
 import { useLayout } from '@/layout/composables/layout';
 import { useRouter } from 'vue-router';
+import { useCountdownStore } from '@/store/countdown';
+import imageUrl from '@/assets/images/LogoDMBranco.png';
+import MyMenu from './myMenu.vue';
+import VueCountdown from '@chenfengyuan/vue-countdown';
+import { useAuthStore } from '@/store/authStore.js';
 
 const { onMenuToggle } = useLayout();
 
 const outsideClickListener = ref(null);
 const topbarMenuActive = ref(true);
 const router = useRouter();
+const store = useAuthStore();
+const nome = store.userName;
+const role = store.userRole;
+
+const countdownStore = useCountdownStore();
+const millisecondsRemaining = countdownStore.millisecondsRemaining;
+
 onMounted(() => {
     bindOutsideClickListener();
+
+    if (millisecondsRemaining > 0) {
+        startCountdown();
+    }
 });
 
 onBeforeUnmount(() => {
     unbindOutsideClickListener();
 });
-
-
 
 const onTopBarMenuButton = () => {
     topbarMenuActive.value = !topbarMenuActive.value;
@@ -43,23 +57,69 @@ const isOutsideClicked = (event) => {
 
     const topbarEl = document.querySelector('.layout-topbar-menu-button');
 
-    return !(topbarEl===(event.target) || topbarEl.contains(event.target));
+    return !(topbarEl === event.target || topbarEl.contains(event.target));
 };
+
+function startCountdown() {
+    return true;
+}
+
+function onCountdownEnd() {
+    store.logout();
+    router.push({ name: 'login' });
+}
+    
+function padZero(value) {
+    return String(value).padStart(2, '0');
+}
 </script>
 
 <template>
-    <div class="layout-topbar">
+    <!-- logo e hamburguer -->
+    <div class="flex layout-topbar justify-content-between align-items-center">
+        <div class="flex align-items-center">
+            <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
+                <i class="pi pi-bars"></i>
+            </button>
+            <button class="p-link layout-topbar-menu-button layout-topbar-button" @click="onTopBarMenuButton()">
+                <i class="pi pi-ellipsis-v"></i>
+            </button>
+            <Image :src="imageUrl" width="250" class="mt-12" />
+        </div>
 
-
-        <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
-            <i class="pi pi-bars"></i>
-        </button>
-
-        <button class="p-link layout-topbar-menu-button layout-topbar-button" @click="onTopBarMenuButton()">
-            <i class="pi pi-ellipsis-v"></i>
-        </button>
-
+        <!-- usuário, role -->
+        <div class=" flex align-items-center">
+            <div class="formgrid grid flex align-items-center">
+                <div class="mt-3 field col-3">
+                    <Avatar icon="pi pi-user" class="" size="xlarge" shape="circle" />
+                </div>
+                <div class="field col-6 ml-3">
+                    <h6 class="usuario mt-3 m-0">{{ nome }}</h6>
+                    <span class='role text-left' style="color: rgba(255,255,255,.5)">{{ role }}</span>
+                </div>
+            </div>
+        </div>
+        <!-- relogio -->
+        <div class="relogio ml-3">
+                <vue-countdown :time="millisecondsRemaining" v-slot="{ minutes, seconds }" @start="startCountdown" @end="onCountdownEnd">{{ padZero(minutes) }}:{{ padZero(seconds) }}</vue-countdown>
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.relogio {
+    font-size: 22pt;
+    font-weight: bold;
+    color: white;
+    padding-top: 0px;
+    margin-left: 10px;
+}
+
+.usuario {
+    width: 110px;
+}
+
+.role {
+    color: rgba(255, 255, 255, 0.5);
+}
+</style>
--- src/layout/mysidebar.vue
+++ src/layout/mysidebar.vue
@@ -4,67 +4,53 @@ import VueCountdown from '@chenfengyuan/vue-countdown';
 import imageUrl from '@/assets/images/LogoDMBranco.png';
 import { ref, onMounted } from 'vue';
 import { useAuthStore } from '@/store/authStore.js';
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 import { useCountdownStore } from '@/store/countdown';
 
 const store = useAuthStore();
-const router = useRouter()
+const router = useRouter();
 const nome = store.userName;
 const role = store.userRole;
 
 const countdownStore = useCountdownStore();
-const millisecondsRemaining = countdownStore.millisecondsRemaining
-
+const millisecondsRemaining = countdownStore.millisecondsRemaining;
 
 function startCountdown() {
-   return true;
+    return true;
 }
-
 
 function onCountdownEnd() {
-//   counting.value = false;
-  store.logout();
-  router.push({ name: 'login'})
+    //   counting.value = false;
+    store.logout();
+    router.push({ name: 'login' });
 }
 onMounted(() => {
-  if (millisecondsRemaining > 0) {
-    startCountdown();  
-  }
+    if (millisecondsRemaining > 0) {
+        startCountdown();
+    }
 });
 </script>
 
 <template>
-    <Image :src="imageUrl" width="250" class="mt-12" />
-    <div class="relogio justify-content text-center p-4">
-        <vue-countdown :time="millisecondsRemaining" v-slot="{ minutes, seconds }"
-                   @start="startCountdown" @end="onCountdownEnd">
-      {{ minutes }}:{{ seconds }}
-    </vue-countdown>
-        <!-- <vue-countdown :time="millisecondsRemaining" v-slot="{ minutes, seconds }"
-         @start="startCountdown" @end="onCountdownEnd">
-            {{ minutes }} : {{ seconds }}
-        </vue-countdown>     -->
-    </div>
     <div class="formgrid grid">
         <div class="field col-3">
             <Avatar icon="pi pi-user" class="mr-2" size="xlarge" shape="circle" />
         </div>
-        <div class="field col-6 mt-3" >
+        <div class="field col-6 mt-3">
             <h6 class="m-0">{{ nome }}</h6>
-            <span style="color: rgba(255,255,255,.5)">{{ role }}</span>
+            <span style="color: rgba(255, 255, 255, 0.5)">{{ role }}</span>
         </div>
-    
     </div>
     <MyMenu></MyMenu>
 </template>
 
 <style lang="scss" scoped>
-.relogio{
+.relogio {
     font-size: 22pt;
     font-weight: bold;
-    color:white;
+    color: white;
 }
-.span{
-    color:rgba(255,255,255,.5)
+.span {
+    color: rgba(255, 255, 255, 0.5);
 }
 </style>
