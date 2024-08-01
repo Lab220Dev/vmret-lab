361f68aa4cc9589e335ece1519856bae5340b4ab
Lab220Dev <suporte@lab220.com.br>
2024-08-01 09:13:59 -0300

    FichasRetiradas
    Só alterei o tamanho para telas xl

--- src/views/relatorios/fichasretiradas.vue
+++ src/views/relatorios/fichasretiradas.vue
@@ -168,31 +168,31 @@ onMounted(() => {
                 <h5 class="my-4 text-2xl">Fichas Retiradas</h5>
                 <div class="p-0 m-0 p-fluid formgrid grid col-12" v-if="show">
                     <!-- div de busca de informações para o relatorio -->
-                    <div class="field lg:col-6 md:col-6 sm:col-6">
+                    <div class="field xl:col-3 lg:col-6 md:col-6 sm:col-6">
                         <label for="planta">Planta:</label>
                         <Dropdown class="drop" v-model="relatorio.id_planta" 
                         :options="plantas" optionLabel="label" optionValue="value" placeholder="Todos"
                         ref="dropdown1"/>
                     </div>
-                    <div class="field lg:col-6 md:col-6 sm:col-6">
+                    <div class="field xl:col-3 lg:col-6 md:col-6 sm:col-6">
                         <label for="perfil">Funcionário:</label>
                         <Dropdown class="drop" v-model="relatorio.id_funcionario" 
                         :options="ListaFuncionarios" optionLabel="label" optionValue="value"
                         ref="dropdown2" placeholder="Todos"/>
                     </div>
-                    <div class="field datepicker lg:col-4 md:col-6 sm:col-6">
+                    <div class="field datepicker xl:col-2 lg:col-4 md:col-6 sm:col-6">
                         <label for="perfil">Data Inicial:</label>
                         <VueDatePicker class=" drop" v-model="relatorio.data_inicio" showIcon :showOnFocus="false" 
                         :format="format" locale="pt-BR" auto-apply :enable-time-picker="false"
                         @open="handleDatepickerOpen" placeholder="Selecione uma data"/>
                     </div>
-                    <div class="field lg:col-4 md:col-6 sm:col-6">
+                    <div class="field xl:col-2 lg:col-4 md:col-6 sm:col-6">
                         <label for="perfil">Data Final:</label>
                         <VueDatePicker class="datepicker" v-model="relatorio.data_final" showIcon :showOnFocus="false" 
                         :format="format" locale="pt-BR" auto-apply :enable-time-picker="false" placeholder="Selecione uma data"
                         @open="handleDatepickerOpen"/>
                     </div>
-                    <div class="field lg:col-4 md:col-6 sm:col-6">
+                    <div class="field xl:col-2 lg:col-4 md:col-6 sm:col-6">
                         <!-- botão de filtrar -->
                         <Button class="filtrar" type="button" label="Filtrar Dados" icon="pi pi-search" severity="info" @click="buscar" />
                     </div>
