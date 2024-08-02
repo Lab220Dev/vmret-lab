27195e97df636da281b4f1f24589ee34f3ce06b8
Lab220Dev <suporte@lab220.com.br>
2024-08-02 17:30:13 -0300

    HomeView e Web
    Ajuste de margem no home view e ajuste de responsividade no Usuário Web

--- src/views/cadastros/Clientes.vue
+++ src/views/cadastros/Clientes.vue
@@ -111,12 +111,7 @@ watch(active, (newIndex, oldIndex) => {
     }
 });
 const resetForm = () => {
-    cliente.nome = '',
-        cliente.cpfcnpj = '',
-        cliente.ativo = true,
-        cliente.created = new Date(),
-        cliente.usarApi = false,
-        cliente.textoretirada = ''
+    (cliente.nome = ''), (cliente.cpfcnpj = ''), (cliente.ativo = true), (cliente.created = new Date()), (cliente.usarApi = false), (cliente.textoretirada = '');
 };
 const loadCliente = async () => {
     loading.value = true;
@@ -150,7 +145,7 @@ const formatDate = (value) => {
         const localDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
 
         const day = String(localDate.getDate()).padStart(2, '0');
-        const month = String(localDate.getMonth() + 1).padStart(2, '0'); 
+        const month = String(localDate.getMonth() + 1).padStart(2, '0');
         const year = localDate.getFullYear();
         const hours = String(localDate.getHours()).padStart(2, '0');
         const minutes = String(localDate.getMinutes()).padStart(2, '0');
@@ -164,7 +159,6 @@ const formatDate = (value) => {
 onMounted(() => {
     loadCliente();
 });
-
 </script>
 
 <template>
@@ -172,25 +166,33 @@ onMounted(() => {
         <TabView v-model:activeIndex="active">
             <TabPanel header="Listar Clientes">
                 <div class="col-12">
-                    <DataTable  v-model:filters="filters" :value="ListaClientes" selectionMode="single" tableStyle="min-width: 25%"
-                        :rowsPerPageOptions="[5, 10, 20, 50]" stripedRows dataKey="id" :metaKeySelection="false"
-                        @rowSelect="onRowSelect" :globalFilterFields="['id_cliente','nome','last_login']">
+                    <DataTable
+                        v-model:filters="filters"
+                        :value="ListaClientes"
+                        selectionMode="single"
+                        tableStyle="min-width: 25%"
+                        :rowsPerPageOptions="[5, 10, 20, 50]"
+                        stripedRows
+                        dataKey="id"
+                        :metaKeySelection="false"
+                        @rowSelect="onRowSelect"
+                        :globalFilterFields="['id_cliente', 'nome', 'last_login']"
+                    >
                         <template #header>
-                                    <div class="flex justify-content-end">
-                                        <IconField iconPosition="left">
-                                            <InputIcon>
-                                                <i class="pi pi-search" />
-                                            </InputIcon>
-                                            <InputText v-model="filters['global'].value" placeholder="Busca" />
-                                        </IconField>
-                                    </div>
-                                </template>
+                            <div class="flex justify-content-end">
+                                <IconField iconPosition="left">
+                                    <InputIcon>
+                                        <i class="pi pi-search" />
+                                    </InputIcon>
+                                    <InputText v-model="filters['global'].value" placeholder="Busca" />
+                                </IconField>
+                            </div>
+                        </template>
                         <Column field="id_cliente" header="Id"></Column>
                         <Column field="nome" header="Nome"></Column>
                         <Column field="ativo" header="Ativo">
                             <template #body="{ data }">
-                                <i class="pi"
-                                    :class="{ 'pi-check-circle text-green-500 ': data.ativo, 'pi-times-circle text-red-500': !data.ativo }"></i>
+                                <i class="pi" :class="{ 'pi-check-circle text-green-500 ': data.ativo, 'pi-times-circle text-red-500': !data.ativo }"></i>
                             </template>
                         </Column>
                         <Column field="last_login" header="Último Login">
@@ -200,8 +202,7 @@ onMounted(() => {
                         </Column>
                         <Column style="min-width: 8rem">
                             <template #body="slotProps">
-                                <Button icon="pi pi-trash" outlined rounded severity="danger"
-                                    @click="deleteClientedes(slotProps.data)" />
+                                <Button icon="pi pi-trash" outlined rounded severity="danger" @click="deleteClientedes(slotProps.data)" />
                             </template>
                         </Column>
                     </DataTable>
@@ -213,35 +214,33 @@ onMounted(() => {
                         <div class="card">
                             <form @submit.prevent="submitForm">
                                 <div class="p-fluid formgrid grid m-0 p-0">
-                                    <div class="full lg:col-12 md:col-12 sm:col-12">
-                                        <label for="id_planta">Nome:</label>
-                                        <InputText class="my-2" id="id_planta" v-model="cliente.nome" required />
+                                    <div class="full lg:col-6 md:col-12 sm:col-12">
+                                        <div class=""><label for="id_planta">Nome:</label> <InputText class="my-2" id="id_planta" v-model="cliente.nome" required /></div>
+
+                                        <div class="mt-4">
+                                            <label for="cpfcnpj">CNPJ/CPF:</label>
+                                            <InputText class="my-2" id="cpfcnpj" v-model="cliente.cpfcnpj" required />
+                                        </div>
                                     </div>
-                                    <div class="full lg:col-12 md:col-12 sm:col-12">
-                                        <label for="cpfcnpj">CNPJ/CPF:</label>
-                                        <InputText class="my-2" id="cpfcnpj" v-model="cliente.cpfcnpj" required />
-                                    </div>
+
                                     <div class="full lg:col-6 md:col-6 sm:col-6">
                                         <label for="codigo">Menção Ficha Retirada:</label>
-                                        <Textarea v-model="cliente.textoretirada" class="my-2 overflow-scroll" rows="5"
-                                            cols="30" />
+                                        <Textarea style="overflow: hidden; max-height: 200px; min-width: 500px" v-model="cliente.textoretirada" class="my-2" rows="5" cols="30" />
                                     </div>
-                                    <InputSwitch class="grid mt-3 ml-3" v-model="cliente.usarApi" inputId="switch1" />
-                                    <label class="mt-3 ml-4" for="switch1">Tem integração?</label>
-                                    <InputSwitch class="grid mt-3 ml-3" v-model="cliente.ativo" inputId="switch2" />
-                                    <label class="mt-3 ml-4" for="switch2">Cliente Ativo?</label>
+                                    <div class="full xl:col-4 flex flex-column align-items-center m-0 lg:col-4 md:col-4 sm:col-12">
+                                        
+                                        <label class="mt-3 ml-4" for="switch1">Tem integração?</label>
+                                        <InputSwitch class="grid mt-3 ml-3" v-model="cliente.usarApi" inputId="switch1" />
+                                        
+                                        
+                                    </div>
+
+                                    <div class="full xl:col-4 flex flex-column align-items-center m-0 lg:col-4 md:col-4 sm:col-12"><label class="mt-3 ml-4" for="switch2">Cliente Ativo?</label><InputSwitch class="grid mt-3 ml-3" v-model="cliente.ativo" inputId="switch2" /></div>
                                 </div>
                                 <div class="mr-1 mt-4 grid justify-content-end">
-                                    <Button v-if="visible" style="width: 15%;"
-                                        class="flex align-items-center justify-content-center m-2 mr-0"
-                                        label="Atualizar" icon="pi pi-refresh" severity="primary"
-                                        @click="atualizarCliente" />
-                                    <Button style="width: 15%;"
-                                        class="flex align-items-center justify-content-center m-2 mr-0" label="Voltar"
-                                        icon="pi pi-arrow-left" severity="primary" @click="active = 0" />
-                                    <Button v-if="!visible" style="width: 15%;"
-                                        class="flex align-items-center justify-content-center m-2 mr-0" label="Salvar"
-                                        icon="pi pi-check" severity="info" @click="adicionarCliente" />
+                                    <Button v-if="visible" style="width: 15%" class="flex align-items-center justify-content-center m-2 mr-0" label="Atualizar" icon="pi pi-refresh" severity="primary" @click="atualizarCliente" />
+                                    <Button style="width: 15%" class="flex align-items-center justify-content-center m-2 mr-0" label="Voltar" icon="pi pi-arrow-left" severity="primary" @click="active = 0" />
+                                    <Button v-if="!visible" style="width: 15%" class="flex align-items-center justify-content-center m-2 mr-0" label="Salvar" icon="pi pi-check" severity="info" @click="adicionarCliente" />
                                 </div>
                             </form>
                         </div>
@@ -249,13 +248,12 @@ onMounted(() => {
                 </div>
             </TabPanel>
         </TabView>
-        <Dialog header="Deletar Cliente" v-model:visible="deleteClienteDialog" style="width: 400px" :modal="true"
-            :closable="false">
+        <Dialog header="Deletar Cliente" v-model:visible="deleteClienteDialog" style="width: 400px" :modal="true" :closable="false">
             <div class="confirmation-content">
                 <i class="pi pi-exclamation-triangle mr-1" style="font-size: 2rem"></i>
                 <span class="">
-                    Você tem certeza que deseja deletar o Cliente <b>{{ item.id_cliente }}</b> - <b>{{ item.nome }}</b>
-                    ?</span>
+                    Você tem certeza que deseja deletar o Cliente <b>{{ item.id_cliente }}</b> - <b>{{ item.nome }}</b> ?</span
+                >
             </div>
             <template #footer>
                 <Button label="Não" icon="pi pi-times" @click="deleteFuncionarioDialog = false" class="p-button-text" />
