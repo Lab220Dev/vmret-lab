822a76f86e2deadf2f7a501ae62a416780880b38
Gabriel Ramos <gtorres321@gmail.com>
2024-08-14 17:35:34 -0300

    Revert "Update funcionarios.vue"
    This reverts commit c7483d79fd4c959178ac1e2b5c26981c7caa21dd.

--- src/views/cadastros/funcionarios.vue
+++ src/views/cadastros/funcionarios.vue
@@ -337,7 +337,7 @@ const setTempo = (tempoRef, isoString) => {
 
 const validateForm = () => {
     errors.value = {};
-    validateCPF();
+    cpfvalidate();
     validateEmail();
     return Object.keys(errors.value).length === 0;
 };
