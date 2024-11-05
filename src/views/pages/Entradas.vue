befbd3431a1535890cf1f3a62b452b414f0d14b4
Gabriel Ramos <gtorres321@gmail.com>
2024-11-05 11:54:56 -0300

    Entradas
    Entrada: Atualizações relacionadas a recuperar e atualizar dados para conexão externas

--- src/views/pages/Entradas.vue
+++ src/views/pages/Entradas.vue
@@ -1,42 +1,121 @@
 <script setup>
-import { reactive } from 'vue';
+import { ref,reactive ,onMounted} from 'vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import { useAuthStore } from '@/store/authStore.js';
+import axios from '@/axios.js';
+import { useToast } from 'primevue/usetoast';
+const toast = useToast();
 
+const store = useAuthStore();
+const DMSelecionada = ref(null)
+const loading = ref(false);
+const Dados = ref([]);
 const Integracao = reactive({
-    Integracao: false,
-    UserID: '',
-    senha: '',
-    ClienteID: '',
-    Chave: ''
+    ClienteID:'',
+    UserID:'',
+    URL:'',
+    Chave:'',
+    ChaveAPI:''
 });
 
-// Função de salvar
-const salvarIntegracao = () => {
-    // Lógica para salvar os dados da integração
-    console.log('Dados de Integração salvos:', Integracao);
-    // Aqui você pode adicionar uma chamada para salvar os dados no backend
-    // Exemplo:
-    // axios.post('/sua-rota-de-api', Integracao)
-    //      .then(response => console.log('Sucesso:', response))
-    //      .catch(error => console.error('Erro:', error));
+const fetchDadosIniciais = async () => {
+    loading.value = true;
+    try {
+        const data = {
+            id_cliente: store.userIdCliente,
+            id_usuario: store.userId
+        };
+        const response = await axios.post('/DM/recuperarInfo', data);
+        Dados.value = response.data;
+    } catch (error) {
+        console.error('Erro ao carregar Itens:', error);
+    } finally {
+        loading.value = false;
+    }
 };
+
+const handleDMChange = () =>{
+    const selectedDM = Dados.value.find((c) => c.ID_DM === DMSelecionada.value);
+    if (selectedDM) {
+        Integracao.UserID=selectedDM.UserID;
+        Integracao.URL=selectedDM.URL;
+        Integracao.ClienteID=selectedDM.ClienteID;
+        Integracao.Chave=selectedDM.Chave;
+        Integracao.ChaveAPI=selectedDM.ChaveAPI;
+    }
+}
+const salvarIntegracao = async () => {
+    try {
+        let data ={
+            id_cliente: store.userIdCliente,
+            ID_DM: DMSelecionada.value,
+            ClienteID: Integracao.ClienteID,
+            UserID: Integracao.UserID,
+            URL: Integracao.URL,
+            Chave: Integracao.Chave,
+            ChaveAPI: Integracao.ChaveAPI
+        }
+        loading.value = true;
+
+        const response = await axios.post('/DM/updateInfo', data);
+        if (response.status === 200 || response.status === 201) {
+            toast.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Dados de integração salvos com sucesso!',
+                life: 3000
+            });
+        } else {
+            throw new Error('Falha ao salvar dados de integração');
+        }
+    } catch (error) {            
+        toast.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: `${error.response?.data?.message || 'Erro ao salvar dados de integração'}`,
+            life: 3000
+        });
+        console.error('Erro ao salvar dados de integração:', error);
+    } finally {
+        loading.value = false;
+    }
+};
+onMounted(() => {
+    fetchDadosIniciais();
+});
 </script>
 
 <template>
     <div class="card" >
         <h2 class="my-6 ml-2 text-2xl">Configurações de Integração</h2>
+            <Dropdown
+                :options="Dados"
+                :virtualScrollerOptions="{ itemSize: 30 }"
+                :filter="true"
+                :filterBy="'Identificacao'"
+                v-model="DMSelecionada"
+                optionLabel="Identificacao"
+                optionValue="ID_DM"
+                placeholder="Selecione uma DM"
+                @change="handleDMChange()"
+            />
         <form @submit.prevent="salvarIntegracao">
             <div class="p-fluid grid">
                 <div class="mt-4 lg:col-6 md:col-6 sm:col-12">
                     <label for="userid">UserID API:</label>
-                    <InputText class="my-2" id="userid" v-model="Integracao.UserID" type="password" />
+                    <InputText class="my-2" id="userid" v-model="Integracao.UserID" type="text" />
                 </div>
                 <div class="mt-4 lg:col-6 md:col-6 sm:col-12">
-                    <label for="senha">Senha API:</label>
-                    <InputText class="my-2" id="senha" v-model="Integracao.senha" type="password" />
+                    <label for="senha">URL API:</label>
+                    <InputText class="my-2" id="senha" v-model="Integracao.URL" type="text"/>
                 </div>
                 <div class="lg:col-6 md:col-6 sm:col-12">
                     <label for="idcliente">IdCliente API:</label>
-                    <InputText class="my-2" id="idcliente" v-model="Integracao.ClienteID" type="password" />
+                    <InputText class="my-2" id="idcliente" v-model="Integracao.ClienteID" type="text" />
+                </div>                
+                <div class="lg:col-6 md:col-6 sm:col-12">
+                    <label for="chaveapi">senha API:</label>
+                    <InputText class="my-2" id="chaveapi" v-model="Integracao.ChaveAPI" type="text" />
                 </div>
                 <div class="lg:col-6 md:col-6 sm:col-12">
                     <label for="chave">Chave:</label>
@@ -49,6 +128,7 @@ const salvarIntegracao = () => {
             </div>
         </form>
     </div>
+    <LoadingSpinner v-if="loading" />
 </template>
 
 <style scoped>
