631557971bd7ed293fdd7cc7dd9e9624167a9a04
Lab220Dev <suporte@lab220.com.br>
2025-01-15 09:36:35 -0300

    Update funcionarioService.js

--- src/Services/funcionarioService.js
+++ src/Services/funcionarioService.js
@@ -1,7 +1,6 @@
 import axios from '@/axios.js';
 import { useAuthStore } from '@/store/authStore.js';
 import { prepareFuncionarioData, prepareprodutoData } from '@/helpers/HelperFuncionario';
-import { prepareFuncionarioData, prepareprodutoData } from '@/helpers/HelperFuncionario';
 const funcionarioService = {
     async listarFuncionarios(data) {
         return axios.post('/funcionarios/listar', data);
